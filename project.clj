5dee0ba4357165f048e42d02680a2296587eb584
Aphyr <aphyr@aphyr.com>
2013-07-16 08:53:24 -0700

    0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-librato "0.0.3"
+(defproject clj-librato "0.0.4-SHAPSHOT"
   :description "Clojure interface to the Librato service"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.5"]
